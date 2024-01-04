e0c1f245f3a240dc3f05328f3690d13695c60714
mahmud <42598394+mahmudtitumir@users.noreply.github.com>
2024-01-04 16:35:27 +0000

    tring clean code

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 let backdrop = document.querySelector(".backdrop");
 let hamburgerMenu = document.querySelector(".toggle-button");
 let mobileNav = document.querySelector(".mobile-nav");
+let aboutScroll = document.getElementById("contact");
 
 backdrop.addEventListener("click", function () {
   mobileNav.style.display = "none";
@@ -14,4 +15,9 @@ function closeModal() {
 hamburgerMenu.addEventListener("click", function () {
   mobileNav.style.display = "block";
   backdrop.style.display = "block";
+});
+
+console.log(aboutScroll);
+aboutScroll.scrollIntoView({
+  behavior: "smooth"
 });
